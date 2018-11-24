bbe619beb3f54b8bc6deefefdf34c97fb9e7f21f
Georgi Borisov <gbbozhilov@abv.bg>
2018-11-24 16:38:07 +0200

    Add the whole blog

---
+++ 05. EXERCISE FRAMEWORK INTRODUCTION/SymfonyBlog/src/SoftUniBlogBundle/Entity/Article.php
@@ -0,0 +1,216 @@
+<?php
+
+namespace SoftUniBlogBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Article
+ *
+ * @ORM\Table(name="articles")
+ * @ORM\Entity(repositoryClass="SoftUniBlogBundle\Repository\ArticleRepository")
+ */
+class Article
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="content", type="text")
+     */
+    private $content;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="dateAdded", type="datetime")
+     */
+    private $dateAdded;
+
+    /**
+     * @var string
+     */
+    private $summary;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="authorId", type="integer")
+     */
+    private $authorId;
+
+    /**
+     * @var User
+     *
+     * @ORM\ManyToOne(targetEntity="SoftUniBlogBundle\Entity\User", inversedBy="articles")
+     * @ORM\JoinColumn(name="authorId", referencedColumnName="id")
+     */
+    private $author;
+
+
+    /**
+     * Article constructor.
+     */
+    public function __construct()
+    {
+        $this->dateAdded = new \DateTime('now');
+    }
+
+
+    /**
+     * Get id.
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title.
+     *
+     * @param string $title
+     *
+     * @return Article
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title.
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set content.
+     *
+     * @param string $content
+     *
+     * @return Article
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    /**
+     * Get content.
+     *
+     * @return string
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * Set dateAdded.
+     *
+     * @param \DateTime $dateAdded
+     *
+     * @return Article
+     */
+    public function setDateAdded($dateAdded)
+    {
+        $this->dateAdded = $dateAdded;
+
+        return $this;
+    }
+
+    /**
+     * Get dateAdded.
+     *
+     * @return \DateTime
+     */
+    public function getDateAdded()
+    {
+        return $this->dateAdded;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSummary(): ?string
+    {
+        if (\strlen($this->content) > 50) {
+            $this->setSummary();
+        }
+
+        return $this->summary;
+    }
+
+    /**
+     * @internal param string $summary
+     */
+    public function setSummary()
+    {
+        $halfLength = \strlen($this->getContent()) / 2;
+        $this->summary = substr($this->getContent(), 0, $halfLength) . '...';
+    }
+
+    /**
+     * @return int
+     */
+    public function getAuthorId(): int
+    {
+        return $this->authorId;
+    }
+
+    /**
+     * @param int $authorId
+     * @return Article
+     */
+    public function setAuthorId(int $authorId): Article
+    {
+        $this->authorId = $authorId;
+
+        return $this;
+    }
+
+    /**
+     * @return User
+     */
+    public function getAuthor(): User
+    {
+        return $this->author;
+    }
+
+    /**
+     * @param User $author
+     *
+     * @return Article
+     */
+    public function setAuthor(User $author = null): Article
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+}
