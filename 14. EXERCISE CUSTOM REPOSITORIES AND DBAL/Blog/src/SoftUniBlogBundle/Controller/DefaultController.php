e818bc296a621778b8ada330edb67faf76c914ee
Georgi Borisov <gbbozhilov@abv.bg>
2019-01-01 21:34:03 +0200

    Add new folder

---
+++ 14. EXERCISE CUSTOM REPOSITORIES AND DBAL/Blog/src/SoftUniBlogBundle/Controller/DefaultController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace SoftUniBlogBundle\Controller;
+
+use SoftUniBlogBundle\Entity\Article;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="blog_index")
+     */
+    public function indexAction()
+    {
+        $articles = $this
+            ->getDoctrine()
+            ->getRepository(Article::class)
+            ->findBy([], ['viewCount' => 'desc', 'dateAdded' => 'desc']);
+
+        return $this->render('default/index.html.twig',
+            ['articles' => $articles]);
+    }
+}
